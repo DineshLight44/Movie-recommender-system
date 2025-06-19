73576a9c74157e1f0eeec75ef685c46e1a999e1f
Dinesh <dineshlodhiglt@gmail.com>
2025-06-19 14:20:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 19 14:10:42 2025
+
+@author: dkglt
+"""
+
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Load model and data
+import pickle
+
+df = pickle.load(open(r'D:\datasets\movies\movies.pkl', 'rb'))
+similarity = pickle.load(open(r'D:\datasets\movies\similarity.pkl', 'rb'))
+
+
+def recommend(movie):
+    movie = movie.lower()
+    if movie not in df['title'].str.lower().values:
+        return ["Movie not found in dataset."]
+    index = df[df['title'].str.lower() == movie].index[0]
+    distances = similarity[index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    return [df.iloc[i[0]].title for i in movie_list]
+
+# Streamlit UI
+st.title('🎥 Movie Recommender System')
+
+# Movie input
+movie_name = st.text_input('Enter a movie name:')
+
+# Recommend button
+if st.button('Recommend'):
+    recommendations = recommend(movie_name)
+    st.write("### Recommended Movies:")
+    for movie in recommendations:
+        st.write("✅ " + movie)
